7320a532baafc6acfd0888bc79f4de5dab3ec083
titouan ottinger <titouan_74@hotmail.fr>
2026-03-28 00:01:07 +0100

    test protection main

--- src/ingestion/main_ingestion_postgre.py
+++ src/ingestion/main_ingestion_postgre.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+#test protection main
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
